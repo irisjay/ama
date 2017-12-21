54e39a331c89fe410453d667a92bef6ce750d356
mumenrider <brian.aquinde@outlook.com>
2017-12-21 18:31:32 +0000

    error handling

--- shell.js
+++ shell.js
@@ -5,7 +5,13 @@ var ama = require ('./ama');
 var write_stdout = function (x) {
     process .stdout .write (x);
 };
+var write_stderr = function (x) {
+    process .stderr .write (x);
+};
 
 
 read_stdin () .then (ama)
-    .then (write_stdout);
+    .then (write_stdout)
+    .catch (function (error) {
+		write_stderr (error .toString ());
+	});
